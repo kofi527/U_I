80e1e88f329755e0a8aac556e40eaf45830d5e45
ABOAGYE KENNETH DAVID <63665356+kofi527@users.noreply.github.com>
2025-07-13 19:40:39 -0500

    Update AddEmployee.jsx
    Adding file to the various components within the navbar

---
+++ src/components/AddEmployee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AddEmployee = props => {
+  return (
+    <div>AddEmployee</div>
+  )
+}
+
+AddEmployee.propTypes = {}
+
+export default AddEmployee
